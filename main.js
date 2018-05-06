1ddab4a734dbbf4ff276b2c0227faf6adf3d8f4c
bidetaggle <axel@vps453651.ovh.net>
2018-05-06 20:46:56 +0200

    new version

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ var pusher = new Pusher('de504dc5763aeef9ff52');
 var channel = pusher.subscribe('live_trades');
 channel.bind('trade', function(data){
 	console.log(data);
-	connection.query(`INSERT INTO btc_usd (id_local, amount, buy_order_id, sell_order_id, price, timestamp, type, id) VALUES ('', '${data.amount}', '${data.buy_order_id}', '${data.sell_order_id}', '${data.price}', '${data.timestamp}', '${data.type}', '${data.id}');`, function (error, results, fields) {
+	connection.query(`INSERT INTO btc_usd (amount, buy_order_id, sell_order_id, price, timestamp, type, id) VALUES ('${data.amount}', '${data.buy_order_id}', '${data.sell_order_id}', '${data.price}', '${data.timestamp}', '${data.type}', '${data.id}');`, function (error, results, fields) {
 		if (error) throw error;
 	});
 });
---
+++ readme.md
@@ -0,0 +1,5 @@
+Please check if the process is not already running before execute it (htop)
+`nohup node main.js &`
+
+logs stored in nohup.out
+`tail -f nohup.out`

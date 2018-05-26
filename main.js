ad5d463a4682ca0bbb60decddbee2c382b3ab51a
bidetaggle <axel@archlinux.localdomain>
2018-05-26 21:15:56 +0300

    id_local added to the console.log

--- main.js
+++ main.js
@@ -7,16 +7,21 @@ var connection = mysql.createConnection({
   password : 'root',
   database : 'bitstamp'
 });
- 
+
 connection.connect();
- 
- 
+
+
 var pusher = new Pusher('de504dc5763aeef9ff52');
 var channel = pusher.subscribe('live_trades');
 channel.bind('trade', function(data){
 	console.log(data);
-	connection.query(`INSERT INTO btc_usd (amount, buy_order_id, sell_order_id, price, timestamp, type, id) VALUES ('${data.amount}', '${data.buy_order_id}', '${data.sell_order_id}', '${data.price}', '${data.timestamp}', '${data.type}', '${data.id}');`, function (error, results, fields) {
+	connection.query(`INSERT INTO btc_usd (amount, buy_order_id, sell_order_id, price, timestamp, type, id) VALUES ('${data.amount}', '${data.buy_order_id}', '${data.sell_order_id}', '${data.price}', '${data.timestamp}', '${data.type}', '${data.id}');`,
+    function (error, results, fields) {
 		if (error) throw error;
+        else {
+            data.id_local = results.insertId;
+            console.log(data);
+        }
 	});
 });
 
